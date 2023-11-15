58492406680fa3352e7126f882db694a25c2de47
KH <KH@DESKTOP-KE662S4>
2023-11-15 19:09:33 +0200

    creating and implementing pokemon interface

---
+++ PokemonReviewApp/Bl/PokemonRepository.cs
@@ -0,0 +1,50 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Bl
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExist(int pokeId);
+    }
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context)
+        {
+            this._context = context;
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemons.FirstOrDefault(x => x.Name == name);
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(x => x.Pokemon.Id == pokeId);
+            if (review.Count() <= 0)
+                return 0;
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(a=>a.Id).ToList();
+        }
+
+        public bool PokemonExist(int pokeId)
+        {
+            return _context.Pokemons.Any(x => x.Id == pokeId);
+        }
+    }
+}
--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PokemonReviewApp;
+using PokemonReviewApp.Bl;
 using PokemonReviewApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // add seed data
 builder.Services.AddTransient<Seed>();
+builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
